3734e35ec32b3a74de988084c85db161d8ec8c57
zydane07 <muhammadzydane3@gmail.com>
2023-07-05 21:19:54 +0700

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
     transpileDependencies: true,
+    devServer: {
+        compress: true,
+        disableHostCheck: true, // That solved it
+    },
 });
